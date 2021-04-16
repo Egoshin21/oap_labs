cbb7a46d2cfc9b2fbefc8458aa0769357f39c392
Egoshin21 <anton.egoshin2016@yandex.ru>
2021-04-16 19:02:06 +0300

    oap_labs

--- oap_labs/Program.cs
+++ oap_labs/Program.cs
@@ -8,36 +8,78 @@ namespace oap_labs
 {
     class Program
     {
-        static void CheckYear(int Year, int Age)
+        static void Main(string[] args)
         {
-            var StartDateTime = new DateTime(Year, 1, 1);
-            do
             {
-                if (ValidDate(StartDateTime, Age))
-                    Console.WriteLine(StartDateTime);
-                StartDateTime = StartDateTime.AddDays(1);
-
-
-            } while (StartDateTime.Year == Year);
-
-
-
-        }
-
-        static bool ValidDate(DateTime CheckedDate, int Age)
-        {
-            var YearSqr = 0;
-            var CurentYear = CheckedDate.Year;
-            while (CurentYear > 0)
+                var MassivDate = new List<DateTime>() {
+                new DateTime(2021, 1, 15),
+                new DateTime(2021, 2, 20),
+                new DateTime(2011, 5, 2),
+                new DateTime(2015, 5, 1),
+                new DateTime(2013, 5, 3),
+            }   new DateTime(2020, 1, 17)};
+            var result = new Dictionary<int, int>();
+            foreach (DateTime TekushayaData in MassivDate)
             {
-                var LastDiget = CurentYear % 10;
-                YearSqr += (int)Math.Pow(LastDiget, 2);
-                CurentYear = (int)Math.Ceiling((double)(CurentYear / 10));
-
+                if (result.ContainsKey(TekushayaData.Month))
+                {
+                    result[TekushayaData.Month] = result[TekushayaData.Month] + 1;
+                }
+                else
+                    result[TekushayaData.Month] = 1;
             }
-            return (YearSqr - (int)Math.Pow(CheckedDate.Day, 2)) == Age;
+            var PopularMonth = -1;
+            var MaxCount = 0;
+            foreach (KeyValuePair<int, int> keyValue in result)
+            {
+                if (keyValue.Value > MaxCount)
+                {
+                    MaxCount = keyValue.Value;
+                    PopularMonth = keyValue.Key;
+                }
+            }
+            Console.WriteLine($"Самый популярный месяц {PopularMonth}");
+
+
+            DateTime date1 = new DateTime(2021, 02, 21);
+            DateTime date2 = new DateTime(2026, 02, 25);
+            Console.WriteLine(Math.Abs(date1.Subtract(date2).TotalDays));
+
+
+            Console.WriteLine("Введите год: ");
+            DateTime date1 = new DateTime(Convert.ToInt32(Console.ReadLine()), 1, 1);
+            date1 = date1.AddDays(255);
+            var DateString = date1.ToString("dd MMMM");
+            Console.WriteLine($"день программиста отмечается  {DateString}");
+
+
+            Console.WriteLine("Введите день: ");
+            DateTime date1 = new DateTime(2021, 1, 1);
+            date1 = date1.AddDays(Convert.ToInt32(Console.ReadLine()) - 1);
+            var DateString = date1.ToString("dddd");
+            var voskresenie = date1.DayOfWeek == DayOfWeek.Sunday ? 7 : (int)date1.DayOfWeek;
+            Console.WriteLine($"день недели {DateString}, и номер дня недели {voskresenie}");
+
+            DateTime Date1 = new DateTime(2021, 06, 21);
+            Console.WriteLine("Введите дату в формате дд.мм.гггг: ");
+            string Date2 = Console.ReadLine();
+            string[] StringArray = Date2.Split('.');
+            var Date3 = new DateTime(Convert.ToInt32(StringArray[2]), Convert.ToInt32(StringArray[1]), Convert.ToInt32(StringArray[0]));
+            var Raznitca = Date1.Subtract(Date3);
+            if (Raznitca.TotalDays == 0)
+            {
+                Console.WriteLine("Сегодня экзамен");
+            }
+            else if (Raznitca.TotalDays > 0)
+                Console.WriteLine($"Экзамен будет через {Raznitca.TotalDays} дней");
+            else
+                Console.WriteLine($"Экзамен был {Math.Abs(Raznitca.TotalDays)} дней назад");
+
+
+            Console.Write("Press ENTER to continue...");
+            Console.ReadLine();
+
 
         }
-
     }
 }
